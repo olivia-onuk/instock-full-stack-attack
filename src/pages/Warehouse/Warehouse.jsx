511cbccff8cd12fc34deeea9960ddb4807e33522
NicoleBiii <xinyuebi23@gmail.com>
2025-03-05 17:37:08 -0500

    style warehouse page hero responsively

--- src/pages/Warehouse/Warehouse.jsx
+++ src/pages/Warehouse/Warehouse.jsx
@@ -16,18 +16,20 @@ function Warehouse() {
     <div className="warehouses-section">
       <div className="warehouses-hero">
         <h1 className="warehouses-title">Warehouses</h1>
-        <div className="warehouse-search">
-          <input 
-          type="text" 
-          className="warehouse-search__input" 
-          placeholder="Search..."
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
-          onKeyDown={(e) => e.key === "Enter" && handleSearch()}
-          />
-          <img src={searchIcon} alt="Search" className="warehouse-search__icon" onClick={handleSearch} />
+        <div className="warehouses-hero__right">
+          <div className="warehouse-search">
+            <input 
+            type="text" 
+            className="warehouse-search__input" 
+            placeholder="Search..."
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+            onKeyDown={(e) => e.key === "Enter" && handleSearch()}
+            />
+            <img src={searchIcon} alt="Search" className="warehouse-search__icon" onClick={handleSearch} />
+          </div>
+          <button className="add-warehouse-button"><h3>+ Add New Warehouse</h3></button>
         </div>
-        <button className="add-warehouse-button"><h3>+ Add New Warehouse</h3></button>
       </div>
       <WarehouseTable />
     </div>
