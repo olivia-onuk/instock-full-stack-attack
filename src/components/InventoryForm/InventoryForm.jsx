9bef4d39cf95c1e106e12c65d3b68ae8a90dcdc8
Tooba Sheikh <tksheikh17@gmail.com>
2025-03-06 22:35:53 -0500

    added the some of the form inputs

--- src/components/InventoryForm/InventoryForm.jsx
+++ src/components/InventoryForm/InventoryForm.jsx
@@ -3,20 +3,36 @@ import arrowBackIcon from "../../assets/icons/arrow_back-24px.svg";
 import "./InventoryForm.scss";
 
 function InventoryForm({ formtype }) {
-  
-const handleSubmit = () =>{
-    
-}  
-  
-return (
+  const handleSubmit = () => {};
+
+  return (
     <form id={formtype} className="form" onSubmit={handleSubmit}>
       <div className="inventory-form-main">
         <div className="inventory-form__column">
-          <h2>Item Details</h2>
-          <div className=""></div>
+          <h2 className="inventory-form__title">Item Details</h2>
+          <div className="inventory-form__input-section">
+            <label className="inventory-form__input-label">Item Name</label>
+            <input type="text" id="itemName" className="inventory-form__input"/>
+          </div>
+          <div className="inventory-form__input-section">
+            <label className="inventory-form__input-label">Description</label>
+            <input type="text" id="itemDescription" className="inventory-form__input inventory-form__input--large"/>
+          </div>
+          <div className="inventory-form__input-section">
+            <label className="inventory-form__input-label">Category</label>
+          </div>
         </div>
         <div className="inventory-form__column">
-          <h2>Item Availability</h2>
+          <h2 className="inventory-form__title">Item Availability</h2>
+          <div className="inventory-form__input-section">
+            <label className="inventory-form__input-label">Status</label>
+          </div>
+          <div className="inventory-form__input-section">
+            <label className="inventory-form__input-label">Quantity</label>
+          </div>
+          <div className="inventory-form__input-section">
+            <label className="inventory-form__input-label">Warehouse</label>
+          </div>
         </div>
       </div>
       <div className="inventory-form__buttons"></div>
