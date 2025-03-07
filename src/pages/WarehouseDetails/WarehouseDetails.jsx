0becf1bc1bcb0f573010382f61eb14199ba96557
NicoleBiii <xinyuebi23@gmail.com>
2025-03-07 17:21:22 -0500

    build WarehouseDetailSection component

--- src/pages/WarehouseDetails/WarehouseDetails.jsx
+++ src/pages/WarehouseDetails/WarehouseDetails.jsx
@@ -1,13 +1,10 @@
+import "./WarehouseDetails.scss";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { Link } from "react-router-dom";
-import { P2 } from "../../components/Typography/Typography";
-import "./WarehouseDetails.scss";
-import InventoryList from "../../components/InventoryList/InventoryList";
 import { fetchWarehouse } from "../../api/ApiService";
-import arrowBackIcon from "../../assets/icons/arrow_back-24px.svg";
-import editIconWhite from "../../assets/icons/edit-white-24px.svg"
 import WarehoudeDetailHero from "../../components/WarehouseDetailHero/WarehoudeDetailHero";
+import WarehouseDetailSection from "../../components/WarehouseDetailSection/WarehouseDetailSection";
+import InventoryList from "../../components/InventoryList/InventoryList";
 
 function WarehouseDetails() {
   const { id } = useParams();
@@ -32,28 +29,7 @@ function WarehouseDetails() {
   return(
     <div className="main warehouse-detail-page">
       <WarehoudeDetailHero id={id} warehouse={warehouse} />
-      <div className="warehouse-detail">
-        <div className="warehouse-detail__address">
-          <h4 className="warehouse-detail__label">WAREHOUSE ADDRESS:</h4>
-          <P2 className="warehouse-detail__address-content"><span>{warehouse.address}, </span><span>{warehouse.city}, {warehouse.country}</span></P2>
-        </div>
-        <div className="warehouse-detail__contact">
-          <div className="warehouse-detail__name">
-            <h4 className="warehouse-detail__label">CONTACT NAME:</h4>
-            <P2>
-              <p>{warehouse.contact_name}</p>
-              <p>{warehouse.contact_position}</p>
-            </P2>
-          </div>
-          <div className="warehouse-detail__infor">
-            <h4 className="warehouse-detail__label">CONTACT INFORMATION:</h4>
-            <P2>
-              <p>{warehouse.contact_phone}</p>
-              <p>{warehouse.contact_email}</p>
-            </P2>
-          </div>
-        </div>
-      </div>
+      <WarehouseDetailSection warehouse={warehouse} />
       <InventoryList id={id} isFullInventory={false}/>
     </div>
   );
