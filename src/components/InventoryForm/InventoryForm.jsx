b3318f55a0d5e5a260af14c71efc63382c88a927
Tooba Sheikh <tksheikh17@gmail.com>
2025-03-07 13:13:18 -0500

    added error checking/styling/form validation  and error text div

--- src/components/InventoryForm/InventoryForm.jsx
+++ src/components/InventoryForm/InventoryForm.jsx
@@ -1,45 +1,121 @@
 import { useState, useEffect } from "react";
 import "./InventoryForm.scss";
+import errorIcon from "../../assets/icons/error-24px.svg"; 
 
-function InventoryForm({ formtype, item, buttonLabel }) {
-  const [error, setError] = useState(false);
+function InventoryForm({
+  formtype,
+  item,
+  buttonLabel,
+  handleUpdate = () => {},
+}) {
+  const [itemName, setItemName] = useState("");
+  const [itemDescription, setItemDescription] = useState("");
+  const [category, setCategory] = useState("");
+  const [qty, setQty] = useState("");
+  const [warehouse, setWarehouse] = useState("");
   const [radio, setRadio] = useState(null);
+  const [error, setError] = useState(false);
 
-  const handleColorChange = (e) => {
+  const handleChangeRadio = (e) => {
     setRadio(e.target.value);
   };
 
-  const handleSubmit = () => {};
+  const handleChangeItemName = (event) => {
+    setItemName(event.target.value);
+  };
 
+  const handleChangeDescription = (event) => {
+    setItemDescription(event.target.value);
+  };
+
+  const handleChangeCategory = (event) => {
+    setCategory(event.target.value);
+  };
+
+  const handleChangeQty = (event) => {
+    setQty(event.target.value);
+  };
+
+  const handleChangeWarehouse = (event) => {
+    setWarehouse(event.target.value);
+  };
+
+  const isFormValid = () => {
+    if (
+      !itemName.trim() ||
+      !itemDescription.trim() ||
+      !category.trim() ||
+      !warehouse.trim() ||
+      !radio
+    ) {
+      return false;
+    } else if (radio == "instock" && !itemDescription.trim()) {
+      return false;
+    }
+    return true;
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (isFormValid()) {
+      try {
+        setError(false);
+      } catch (error) {
+        console.log(error);
+      }
+    } else {
+      setError(true);
+    }
+  };
+  
   return (
     <form id={formtype} className="inventory-form" onSubmit={handleSubmit}>
       <div className="inventory-form__main">
         <div className="inventory-form__column">
           <h2 className="inventory-form__title">Item Details</h2>
           <div className="inventory-form__input-section">
-            <label className="inventory-form__input-label">Item Name</label>
+            <label className="inventory-form__input-label ">Item Name</label>
             <input
               type="text"
               id="itemName"
-              className="inventory-form__input"
+              className={`inventory-form__input ${
+                error && !itemName.trim() ? "inventory-form__input--error" : ""
+              }`}
               placeholder="Item Name"
+              onChange={handleChangeItemName}
             />
+            <p className={`inventory-form__error-text ${
+                error && !itemName.trim()
+                  ? "inventory-form__input--visible"
+                  : ""
+              }`}><img src={errorIcon} alt="error"/>This field is required</p>
           </div>
           <div className="inventory-form__input-section">
             <label className="inventory-form__input-label">Description</label>
             <textarea
               type="text"
               id="itemDescription"
-              className="inventory-form__input inventory-form__input--large"
+              className={`inventory-form__input inventory-form__input--large ${
+                error && !itemDescription.trim()
+                  ? "inventory-form__input--error"
+                  : ""
+              }`}
               placeholder="Please enter a brief description..."
+              onChange={handleChangeDescription}
             />
           </div>
           <div className="inventory-form__input-section">
             <label className="inventory-form__input-label">Category</label>
             <select
-              className="inventory-form__dropdown"
+              className={`inventory-form__dropdown ${
+                error && !category.trim()
+                  ? "inventory-form__dropdown--error"
+                  : ""
+              }`}
               name="category"
               id="category"
+              onChange={handleChangeCategory}
             >
               <option value="" disabled selected hidden>
                 Please Select
@@ -61,8 +137,12 @@ function InventoryForm({ formtype, item, buttonLabel }) {
                 type="radio"
                 name="status"
                 value="instock"
-                className="inventory-form__stock inventory-form__stock--instock"
-                onChange={handleColorChange}
+                className={`inventory-form__stock inventory-form__stock--instock ${
+                  error && !radio
+                    ? "inventory-form__stock--error"
+                    : ""
+                }`}
+                onChange={handleChangeRadio}
               />
               <label
                 className={`inventory-form__radio-label ${
@@ -77,8 +157,12 @@ function InventoryForm({ formtype, item, buttonLabel }) {
                 type="radio"
                 name="status"
                 value="outofstock"
-                className="inventory-form__stock inventory-form__stock--outofstock"
-                onChange={handleColorChange}
+                className={`inventory-form__stock inventory-form__stock--outofstock ${
+                  error && !radio
+                    ? "inventory-form__stock--error"
+                    : ""
+                }`}
+                onChange={handleChangeRadio}
               />
               <label
                 className={`inventory-form__radio-label ${
@@ -93,23 +177,31 @@ function InventoryForm({ formtype, item, buttonLabel }) {
           </div>
           <div
             className={`inventory-form__input-section ${
-              radio === "outofstock" ? "inventory-form__input-section--invisible" : ""
+              radio === "outofstock"
+                ? "inventory-form__input-section--invisible"
+                : ""
             }`}
           >
             <label className="inventory-form__input-label">Quantity</label>
             <input
               type="number"
               id="qty"
-              className="inventory-form__input inventory-form__input--small"
+              className={`inventory-form__input inventory-form__input--small ${error && !qty.trim() ? "inventory-form__input--error" : ""}`}
               placeholder="0"
+              onChange={handleChangeQty}
             />
           </div>
           <div className="inventory-form__input-section">
             <label className="inventory-form__input-label">Warehouse</label>
             <select
-              className="inventory-form__dropdown"
+              className={`inventory-form__dropdown ${
+                error && !warehouse.trim()
+                  ? "inventory-form__dropdown--error"
+                  : ""
+              }`}
               name="warehouse"
               id="warehouse"
+              onChange={handleChangeWarehouse}
             >
               <option value="" disabled selected hidden>
                 Please Select
