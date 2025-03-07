80915b0595048b0aa391439200ce963002502620
olivia-onuk <oliviaonuk@gmail.com>
2025-03-07 11:45:01 -0500

    add link to logo to go back to homepage

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,14 @@
 import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 import logo from "../../assets/logo/InStock-Logo.svg";
 import "./Header.scss";
 
 function Header() {
   return (
     <header className="header">
-      <div className="header__logo">
+      <Link to="/" className="header__logo">
         <img src={logo} alt="InStock Logo" className="header__logo-img" />
-      </div>
+      </Link>
       <nav className="header__nav">
         <NavLink to="/warehouse" className="header__link">
           Warehouses
