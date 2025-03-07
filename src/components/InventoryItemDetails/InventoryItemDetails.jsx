9b6063a6eae5aebbb4c732d6e17dee1c452f7125
olivia-onuk <oliviaonuk@gmail.com>
2025-03-07 09:56:39 -0500

    style inventory item details page across all breakpoints

--- src/components/InventoryItemDetails/InventoryItemDetails.jsx
+++ src/components/InventoryItemDetails/InventoryItemDetails.jsx
@@ -5,6 +5,7 @@ function InventoryItemDetails({ item }) {
 
   return (
     <section className="inventory-item-details">
+      <div className="inventory-item-details__info">
       <div className="inventory-item-details__content">
         <h4>ITEM DESCRIPTION:</h4>
         <p>{item.description}</p>
@@ -12,7 +13,9 @@ function InventoryItemDetails({ item }) {
       <div className="inventory-item-details__content">
         <h4>CATEGORY:</h4>
         <p>{item.category}</p>
+        </div>
       </div>
+      <div className="inventory-item-details__stock">
       <div className="inventory-item-details__section">
         <div className="inventory-item-details__content">
           <h4>STATUS:</h4>
@@ -34,7 +37,8 @@ function InventoryItemDetails({ item }) {
       <div className="inventory-item-details__content">
         <h4>WAREHOUSE:</h4>
         <p>{item.warehouse_name}</p>
-      </div>
+        </div>
+        </div>
     </section>
   );
 }
